1eb46b71c0be14baa3e73ab2dbdcc8c02f7c5b0b
Lautaro Molinas <lautaromolinas@Paulas-MacBook-Pro.local>
2024-06-18 22:31:47 -0300

    added MapaCiudadesTDA

--- src/api/MapaCiudadesTDA.java
+++ src/api/MapaCiudadesTDA.java
@@ -1,5 +1,24 @@
 package api;
 
-public interface ListadoCiudadesTDA {
-    
+public interface MapaCiudadesTDA {
+    void inicializarMapa();
+    void agregarProvincia(String provincia);
+    void eliminarProvincia(String provincia);
+    void agregarCiudad(String provincia, String ciudad);
+    void eliminarCiudad(String provincia, String ciudad);
+    String[] listarProvincias();
+    String[] listarCiudades(String provincia);
+    void agregarRuta(String origen, String destino, int distancia);
+    void eliminarRuta(String origen, String destino);
+    String[] ciudadesVecinas(String ciudad);
+    String[] ciudadesPuente(String ciudadA, String ciudadB);
+    String[] ciudadesPredecesoras(String ciudad);
+    String[] ciudadesExtremo();
+    String[] ciudadesFuertementeConectadas();
+    Ruta caminoMasCorto(String ciudadA, String ciudadB);
+}
+
+class Ruta {
+    String[] ciudadesIntermedias;
+    int distanciaTotal;
 }
